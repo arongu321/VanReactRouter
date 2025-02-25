0c5f6c222771181ec33d0a07ab5d88f6af3595a7
aron.gu321@gmail.com <aron.gu321@gmail.com>
2025-02-25 16:55:01 -0700

    Added vans page with link to React route

--- App.jsx
+++ App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import About from './pages/About';
+import Vans from './pages/Vans';
 import Home from './pages/Home';
 
 export default function App() {
@@ -12,11 +13,13 @@ export default function App() {
                 </Link>
                 <nav>
                     <Link to="/about">About</Link>
+                    <Link to="/vans">Vans</Link>
                 </nav>
             </header>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
+                <Route path="/vans" element={<Vans />} />
             </Routes>
         </BrowserRouter>
     );
