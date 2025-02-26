2dcde86227b2c4a139725d5f4c4acdd59de4ab90
aron.gu321@gmail.com <aron.gu321@gmail.com>
2025-02-25 22:43:09 -0700

    Added navlink to detect active link

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,16 +1,37 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export default function Header() {
     return (
         <header>
-            <Link className="site-logo" to="/">
+            <NavLink className="site-logo" to="/">
                 #VANLIFE
-            </Link>
+            </NavLink>
             <nav>
-                <Link to="/host">Host</Link>
-                <Link to="/about">About</Link>
-                <Link to="/vans">Vans</Link>
+                <NavLink
+                    to="/host"
+                    className={({ isActive }) =>
+                        isActive ? 'active-link' : null
+                    }
+                >
+                    Host
+                </NavLink>
+                <NavLink
+                    to="/about"
+                    className={({ isActive }) =>
+                        isActive ? 'active-link' : null
+                    }
+                >
+                    About
+                </NavLink>
+                <NavLink
+                    to="/vans"
+                    className={({ isActive }) =>
+                        isActive ? 'active-link' : null
+                    }
+                >
+                    Vans
+                </NavLink>
             </nav>
         </header>
     );
