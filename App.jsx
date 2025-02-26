0b8191f2d250c7962016ae25cfafcce8dc8db889
aron.gu321@gmail.com <aron.gu321@gmail.com>
2025-02-25 21:22:38 -0700

    Added React Layout Router to define shared layout to group about, vans, and specific van routes

--- App.jsx
+++ App.jsx
@@ -4,16 +4,19 @@ import About from './pages/About';
 import Vans from './pages/Vans';
 import Home from './pages/Home';
 import VanDetails from './pages/VanDetails';
+import Layout from './components/Layout';
 import './server';
 
 export default function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/vans" element={<Vans />} />
-                <Route path="/vans/:vanId" element={<VanDetails />} />
+                <Route element={<Layout />}>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/vans" element={<Vans />} />
+                    <Route path="/vans/:vanId" element={<VanDetails />} />
+                </Route>
             </Routes>
         </BrowserRouter>
     );
