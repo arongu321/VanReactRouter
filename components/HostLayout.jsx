85f5ef64a0315d7b6914fc0f1185b005e25d120d
aron.gu321@gmail.com <aron.gu321@gmail.com>
2025-02-25 22:50:10 -0700

    Added navlink to detect active link in host layout

--- components/HostLayout.jsx
+++ components/HostLayout.jsx
@@ -1,14 +1,45 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export default function HostLayout() {
+    const activeStyle = {
+        fontWeight: 'bold',
+        textDecoration: 'underline',
+        color: '#161616',
+    };
+
     return (
         <>
             <nav className="host-nav">
-                <Link to="/host">Dashboard</Link>
-                <Link to="/host/income">Income</Link>
-                <Link to="/host/reviews">Reviews</Link>
+                <NavLink
+                    style={({ isActive }) => (isActive ? activeStyle : null)}
+                    // The end prop is used to match the end of the URL
+                    // This is useful when you want to match the exact URL
+                    // and not any sub-routes.
+                    // For example, if the URL is /host/income, the
+                    // /host link would not be active unless you use the end prop.
+                    // This is because the /host/income URL is a sub-route of /host.
+                    // The end prop makes sure that the URL is an exact match.
+                    // If you remove the end prop, the /host link would be active
+                    // when the URL is /host/income.
+                    end
+                    to="/host"
+                >
+                    Dashboard
+                </NavLink>
+                <NavLink
+                    style={({ isActive }) => (isActive ? activeStyle : null)}
+                    to="/host/income"
+                >
+                    Income
+                </NavLink>
+                <NavLink
+                    style={({ isActive }) => (isActive ? activeStyle : null)}
+                    to="/host/reviews"
+                >
+                    Reviews
+                </NavLink>
             </nav>
             <Outlet />
         </>
