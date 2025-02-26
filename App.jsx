ccd4371b83d53e106c387cec99ab329724654db2
aron.gu321@gmail.com <aron.gu321@gmail.com>
2025-02-25 21:12:51 -0700

    Added Header and Layout routes

--- App.jsx
+++ App.jsx
@@ -9,15 +9,6 @@ import './server';
 export default function App() {
     return (
         <BrowserRouter>
-            <header>
-                <Link className="site-logo" to="/">
-                    #VANLIFE
-                </Link>
-                <nav>
-                    <Link to="/about">About</Link>
-                    <Link to="/vans">Vans</Link>
-                </nav>
-            </header>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
