0ab8c2d072ca4b74d843bff3e7391e215da8b3ab
aron.gu321@gmail.com <aron.gu321@gmail.com>
2025-02-26 21:12:33 -0700

    Added conditional rendering to show/hide clear filters button and show selected type filter

--- pages/Vans/Vans.jsx
+++ pages/Vans/Vans.jsx
@@ -48,34 +48,59 @@ export default function Vans() {
             </Link>
         </div>
     ));
+
+    // Filter vans based on query search parameters for type
+    function handleFilterChange(key, value) {
+        setSearchParams((prevParams) => {
+            // If the value is null, remove the key from the search params
+            if (value === null) {
+                prevParams.delete(key);
+
+                // If the value is not null, set the key to the value
+            } else {
+                prevParams.set(key, value);
+            }
+            return prevParams;
+        });
+    }
+
     return (
         <div className="vans-container">
             <h2>Explore our van options</h2>
             <div className="van-list-filter-buttons">
                 <button
-                    onClick={() => setSearchParams({ type: 'simple' })}
-                    className="van-type simple"
+                    onClick={() => handleFilterChange('type', 'simple')}
+                    className={`van-type simple ${
+                        typeFilter === 'simple' ? 'selected' : null
+                    }`}
                 >
                     Simple
                 </button>
                 <button
-                    onClick={() => setSearchParams({ type: 'luxury' })}
-                    className="van-type luxury"
+                    onClick={() => handleFilterChange('type', 'luxury')}
+                    className={`van-type luxury ${
+                        typeFilter === 'luxury' ? 'selected' : null
+                    }`}
                 >
                     Luxury
                 </button>
                 <button
-                    onClick={() => setSearchParams({ type: 'rugged' })}
-                    className="van-type rugged"
+                    onClick={() => handleFilterChange('type', 'rugged')}
+                    className={`van-type rugged ${
+                        typeFilter === 'rugged' ? 'selected' : null
+                    }`}
                 >
                     Rugged
                 </button>
-                <button
-                    onClick={() => setSearchParams({})}
-                    className="van-type clear-filters"
-                >
-                    Clear filters
-                </button>
+
+                {typeFilter ? (
+                    <button
+                        onClick={() => handleFilterChange('type', null)}
+                        className="van-type clear-filters"
+                    >
+                        Clear filters
+                    </button>
+                ) : null}
             </div>
             <div className="vans-list-container">{vanElements}</div>
         </div>
